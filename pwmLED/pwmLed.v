e492a7354a0cea21b04d8101c06abaccb65d4747
Joey Conenna <jconenna@knights.ucf.edu>
2016-06-15 19:40:41 -0400

    Programmable PWM Circuit

--- pwmLED/pwmLed.v
+++ pwmLED/pwmLed.v
@@ -16,20 +16,20 @@ module pwnLed
 	reg  [N-1:0] counter_reg;      // register for counter 
 	wire [N-1:0] counter_next;     // next state output of counter
 
-   wire [3:0] dutyCycle;                  // connections to input from dip switch
+        wire [3:0] dutyCycle;                  // connections to input from dip switch
 	assign dutyCycle = ~sw[3:0];           // lower 4 switches assigned decode duty cycle = dc / 16
 
 	// counter register
 	always @(posedge clk)               // on positive clock edge
-		  counter_reg <= counter_next;   // update register contents to next state logic
+            counter_reg <= counter_next;   // update register contents to next state logic
 
 	// next state logic:  if counter register exceeds reset value, reset to 0, else increment by 1
 	assign counter_next = (counter_reg >= totalCycles) ? 14'b0 : counter_reg + 1;
 							 
-   // assign output logic
+        // assign output logic
 	// output is 1 when counter is less than input duty cycle * multiplier : else 
 	// output is 0 for the remaining of the total cycles before the counter resets to 0
-   assign out = (counter_reg < (dutyCycle * multiplier)) ? 1'b1 : 1'b0;
+        assign out = (counter_reg < (dutyCycle * multiplier)) ? 1'b1 : 1'b0;
 	
 	// route LED output to oscilloscope output
 	assign scope = out;
