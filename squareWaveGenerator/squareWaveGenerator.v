f8edacd402440c1a7b337a6a46ce681cd9b168d5
Joey Conenna <jconenna@knights.ucf.edu>
2016-06-15 19:41:20 -0400

    Programmable Square Wave Generator

--- squareWaveGenerator/squareWaveGenerator.v
+++ squareWaveGenerator/squareWaveGenerator.v
@@ -16,21 +16,21 @@ module squareWaveGenerator
 	reg  [N-1:0] counter_reg;      // register for counter 
 	wire [N-1:0] counter_next;     // next state output of counter
 
-   wire [3:0] m , n;              // connections to input from dip switch
+        wire [3:0] m , n;              // connections to input from dip switch
 	assign m = ~sw[7:4];           // upper 4 switches assigned to decode ON interval multiplier
 	assign n = ~sw[3:0];           // lower 4 switches assigned to decode OFF interval multiplier
 
 	// counter register
 	always @(posedge clk)               // on positive clock edge
-		  counter_reg <= counter_next;   // update register contents to next state logic
+            counter_reg <= counter_next;   // update register contents to next state logic
 
 	// next state logic:  if counter register exceeds current reset value, reset to 0, else increment by 1
 	assign counter_next = (counter_reg >= (cycles * (m + n)) - 1) ? 26'b0 : counter_reg + 1;
 							 
-   // assign output logic
+        // assign output logic
 	// when counter register is counting up in ON interval, set output to 1, else: 
 	// if counter register is counting up in OFF interval, set output to 0
-   assign out = (counter_reg < (cycles * m)) ? 1'b1 : 1'b0;	
+        assign out = (counter_reg < (cycles * m)) ? 1'b1 : 1'b0;	
 	
 	// route LED output to oscilloscope output
 	assign scope = out;
